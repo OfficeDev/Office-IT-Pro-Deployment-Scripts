3c00ec1f9bd1961d777242d7dc17833452b06eb6
Russ Smith <rsmith@valoremconsulting.com>
2015-07-17 13:42:35 -0500

    Merging Get-ModernOfficeApps from Development into Master

---
+++ Get-ModernOfficeApps/Get-ModernOfficeApps.ps1
@@ -0,0 +1,111 @@
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+Param
+(
+    [string[]] $ComputerName = $env:COMPUTERNAME,
+    [PSCredential] $Credentials
+)
+
+
+function Get-ModernOfficeApps {
+<#
+.SYNOPSIS
+Gets a list of Modern Office Apps, versions, and the number of installs for each computer
+
+.DESCRIPTION
+This script will query the local or remote computers and detect if Modern Office Apps are installed. Since
+the Modern apps on installed on a per user basis the script will also include the numbers of users
+who have an instance of the application installed
+
+.PARAMETER ComputerNames
+The list of computer names that you would like limit your search to.
+
+.PARAMETER credentials
+The PSCredentials that are used to invoke commands on the remote computers. 
+Will be prompted for if not provided.
+
+.Example
+./Get-ModernAppsRemotely.ps1
+Gets the list of all the office modern apps that are installed on all computers in your domain.
+You will be prompted for credentials.
+
+.Example
+./Get-ModernAppsRemotely.ps1 -ComputerNames ($myArray)
+Gets the list of all the office modern apps that are installed on the specified computers.
+You will be prompted for credentials.
+
+.Outputs
+Application Name, Version, Number of users who have the application installed, ComputerName
+
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+Param
+(
+    [string[]] $ComputerName = $env:COMPUTERNAME,
+    [PSCredential] $Credentials
+)
+
+begin {
+    $defaultDisplaySet = 'Name','Version', 'NumberOfInstalls', 'ComputerName';
+
+    $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]]$defaultDisplaySet);
+    $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet);
+}
+
+Process
+{
+    $HKLM = [UInt32] "0x80000002";
+    $HKCR = [UInt32] "0x80000000";
+    $HKEY_Users = 2147483651;
+
+	$results = new-object PSObject[] 1;
+
+	foreach($computer in $ComputerName)
+	{
+
+        #Actual Functionality
+        if ($Credentials) {
+           $regProv = Get-Wmiobject -list "StdRegProv" -namespace root\default -computername $computer -Credential $Credentials;
+        } else {
+           $regProv = Get-Wmiobject -list "StdRegProv" -namespace root\default -computername $computer;
+        }
+
+        foreach ($userKey in $regProv.EnumKey($HKEY_Users,"").sNames) {
+
+          $regpath = "Software\Classes\ActivatableClasses\Package";
+          $packagePath = join-path $userKey $regpath;
+
+           foreach ($packageKey in $regProv.EnumKey($HKEY_Users,$packagePath).sNames) {
+               $packageName = $packageKey.Split('_')[0];
+               $packageVersion = $packageKey.Split('_')[1];
+
+               if (!$packageName.ToLower().StartsWith("Microsoft.Office".ToLower())) {
+                  continue;
+               }
+        
+                $exists = $false;
+                foreach ($result in $results) {
+                  if ($result.Name) {
+                     if ($result.Name.ToUpper() -eq $packageName.ToUpper() -and $result.Version -eq $packageVersion -and $result.ComputerName -eq $computer) {
+                         $exists = $true;
+                         $result.NumInstalls += 1;
+                     }
+                  }
+                }
+
+                if (!$exists) {
+                   $object = New-Object PSObject -Property @{ Name=$packageName; Version=$packageVersion; ComputerName=$computer; NumberOfInstalls=1;};
+                   $object | Add-Member MemberSet PSStandardMembers $PSStandardMembers;
+                   $results += $object;
+                }
+
+           }
+
+        }
+
+	}
+
+	return $results;
+}
+}
+
+Get-ModernOfficeApps -ComputerName $ComputerName -Credentials $Credentials;
---
+++ Get-ModernOfficeApps/README_Get-ModernOfficeApps.md
@@ -0,0 +1,34 @@
+#Check for Modern Office Apps
+
+List the Modern Office apps installed on the local or remote computers
+
+###**Pre-requisites**
+
+1. Remote Windows Management Instrumentation (WMI) connectivity and permissions to any remote computers you are querying. 
+
+###Examples
+
+####Check for modern apps installed on remote computers:
+
+1. Open a PowerShell console.
+
+            From the Run dialog type PowerShell
+            
+2. Change the directory to the location where the PowerShell Script is saved.
+
+            Example: cd C:\PowerShellScripts
+            
+2. Run the Script. With no parameters specified the script will return the locally installed Office Version.
+
+           Type . .\Get-ModernOfficeApps.ps1
+
+           By including the additional period before the relative script path you are 'Dot-Sourcing' 
+           the PowerShell function in the script into your PowerShell session which will allow you to 
+           run the function 'Get-ModernOfficeApps' from the console.
+	
+3. Run the script for specified computers or against an array you have created.
+
+            Example: .\Get-ModernOfficeApps.ps1 -ComputerName Client1,Client2
+            
+
+
