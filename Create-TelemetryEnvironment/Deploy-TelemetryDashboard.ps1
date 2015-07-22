ec3468b179a6038ac4296620bdba395c2a6d67b2
michaeldraftz <draftz05@gmail.com>
2015-07-22 09:20:18 -0500

    Broke out the registry function into a separate script because is guaranteed to be run somewhere else.

--- Create-TelemetryEnvironment/Deploy-TelemetryDashboard.ps1
+++ Create-TelemetryEnvironment/Deploy-TelemetryDashboard.ps1
@@ -1364,32 +1364,6 @@ function Enable-DOTNET3{
     }
 }
 
-function Set-TelemetryGPO{
-    #Set GPO Registry values given a GPOName for Telemetry
-    $keyPath = "HKCU\Software\Policies\Microsoft\Office\15.0\osm"
-    $GPO = Get-GPO -Name $GPOName
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "CommonFileShare" -Type String -Value "$commonFileShare"
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag1" -Type String -Value "$commonFileShare"
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag2" -Type String -Value "$commonFileShare"
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag3" -Type String -Value "$commonFileShare"
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag4" -Type String -Value "$commonFileShare"
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "AgentInitWait" -Type DWord -Value 00000001
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Enablelogging" -Type DWord -Value 00000001
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "EnableUpload" -Type DWord -Value 00000001
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "EnableFileObfuscation" -Type DWord -Value 00000000
-
-    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "AgentRandomDelay" -Type DWord -Value 00000000
-}
-
 # SIG # Begin signature block
 # MIIagAYJKoZIhvcNAQcCoIIacTCCGm0CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
---
+++ Create-TelemetryEnvironment/Set-TelemetryGPORegistry.ps1
@@ -0,0 +1,34 @@
+﻿    Param(
+        [Parameter(Mandatory=$true)]
+        [string] $GPOName,
+
+        [Parameter()]
+        [string] $VersionNumber = "15"
+    )
+    
+    Process{
+    #Set GPO Registry values given a GPOName for Telemetry
+    $keyPath = "HKCU\Software\Policies\Microsoft\Office\$VersionNumber.0\osm"
+    $GPO = Get-GPO -Name $GPOName
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "CommonFileShare" -Type String -Value "$commonFileShare"
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag1" -Type String -Value "$commonFileShare"
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag2" -Type String -Value "$commonFileShare"
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag3" -Type String -Value "$commonFileShare"
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Tag4" -Type String -Value "$commonFileShare"
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "AgentInitWait" -Type DWord -Value 00000001
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "Enablelogging" -Type DWord -Value 00000001
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "EnableUpload" -Type DWord -Value 00000001
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "EnableFileObfuscation" -Type DWord -Value 00000000
+
+    Set-GPRegistryValue -Guid $GPO.Id -Key $keyPath -ValueName "AgentRandomDelay" -Type DWord -Value 00000000
+
+    }
