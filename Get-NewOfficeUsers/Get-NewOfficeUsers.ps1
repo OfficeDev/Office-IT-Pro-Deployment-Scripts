2426cb7a0f4c6a981751463c002ced23eb0dff18
michaeldraftz <draftz05@gmail.com>
2015-07-29 15:53:13 -0500

    corrected some comment based help (didn't notice was wrong because of copy-pasta)

--- Get-NewOfficeUsers/Get-NewOfficeUsers.ps1
+++ Get-NewOfficeUsers/Get-NewOfficeUsers.ps1
@@ -4,19 +4,15 @@
     Get a list of users the were licensed after the specified date according to the specified csv
 
     .DESCRIPTION
-    Finds all the MSOLUsers that are licensed with the specified plan and stores them in a csv.
-    It populates an extra field in the csv the specifies the earliest point at which the csv
-    knew the user was licensed (LicensedAsOf field). If a user doesn't show up in the licensed list at a later date
-    the function takes note and populates another field in the csv with that date (DelicensedAsOf)
+    Get a list of users the were licensed after the specified date according to the specified csv.
+    It is important to have run the Update-UserLicenseData.ps1 prior to using this script.
 
-    .PARAMETER ServiceName
-    The Name of the Service Plan that you wish to track licensed users for
+    .PARAMETER CutOffDate
+    The cutoff date for how new you wish the return list of users to be
 
     .PARAMETER CSVPath
-    The Full file path of the CSV you wish the data to be tracked in
-
-    .PARAMETER Credentials
-    Credentials for connecting to MSOL service
+    The Full file path of the CSV with the data (should be the same as the path used for
+    Update-UserLicenseData.ps1).
 
     .Example
     Get-RecentlyLicensedUsers -CutOffDate (Get-Date "2015-7-13) -CSVPath "$env:Public\Documents\LicensedUsers.csv"
