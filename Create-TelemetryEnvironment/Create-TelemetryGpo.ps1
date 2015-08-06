e5d0d11710bd749759c840f41fc3117762c4d9f2
AStigall <astigall@valoremconsulting.com>
2015-08-06 14:08:32 -0500

    Added parameters to Create-TelemetryGpo to specify a Gpo name, SQL server name, and Office version.

--- Create-TelemetryEnvironment/Create-TelemetryGpo.ps1
+++ Create-TelemetryEnvironment/Create-TelemetryGpo.ps1
@@ -1,37 +1,58 @@
-﻿Function Create-TelemetryGpo {
+﻿Param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$GpoName,
+    
+    [Parameter()]
+    [string]$SqlServerName,
+
+    [Parameter()]
+    [string]$OfficeVersion
+)
+
+
 <#
 
-.Synopsis
+.SYNOPSIS
 Create the Telemetry GPO on the Domain Controller
 
-.Description
+.DESCRIPTION
 Creates a group policy that that specifies the 
 Telemetry agent file share location and allows
 the agent to log and upload.
 
-.Example
-Create-TelemetryGpo
-The IT Pro will be prompmpted to type the 
-SQL server name. The IT Pro will then be prompted
-to type the version of Office being used.
+.PARAMETER GpoName
+The name of the GPO to be created.
+
+.PARAMETER SqlServerName
+The name of the SQL server hosting the telemetry database.
+
+.PARAMETER OfficeVersion
+The version of office used in your environment. If a version
+earlier than 2013 is used do not use this parameter.
+
+.EXAMPLE
+./Create-TelemetryGpo -GpoName "Office Telemetry" -SqlServerName SQLExpress -officeVersion 2013
+A GPO named "Office Telemetry" will be created. Registry keys will be
+created to enable telemetry agent logging, uploading, and the commonfileshare 
+path set to \\SQLExpress\TDShared. 
+
+.EXAMPLE
+./Create-TelemetryGpo -GpoName "Office Telemetry" -SqlServerName SQLExpress
+A GPO named "Office Telemetry" will be created.
 
 #>
 
-    Import-Module -Name grouppolicy
- 
-    $gpo = "Office Telemetry"
-    New-GPO -Name $gpo
+    Import-Module -Name grouppolicy 
+    
+    New-GPO -Name $gpoName
+
     $shareName = "TDShared"
-    $SqlServer = Read-Host -Prompt 'Type the SQL Server host name'
-
-    Write-Host "You entered $SqlServer"
-        
-    $officeVersion = Read-Host -Prompt 'If you are using Microsoft Office 2013 type 2013. If you are using Microsoft Office 2016 type 2016.'
-
-    if($officeVersion -eq 2013)
+    
+    if($OfficeVersion -eq 2013)
     {
         Write-Host "Set the Fileshare name"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName CommonFileShare -Type String -Value "\\$SqlServer\$shareName"
+        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName CommonFileShare -Type String -Value "\\$SqlServerName\$shareName"
 
         Write-Host "Enable agent logging"
         Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName Enablelogging -Type Dword -Value 1
@@ -39,17 +60,23 @@ to type the version of Office being used.
         Write-Host "Enable agent data upload"
         Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName EnableUpload -Type Dword -Value 1
     }
+        elseif($OfficeVersion -eq 2016)
+        {
+            Write-Host "Set the Fileshare name"
+            Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName CommonFileShare -Type String -Value "\\$SqlServerName\$shareName"
+
+            Write-Host "Enable agent logging"
+            Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName Enablelogging -Type Dword -Value 1
+
+            Write-Host "Enable agent data upload"
+            Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName EnableUpload -Type Dword -Value 1
+        }
     else
     {
-        Write-Host "Set the Fileshare name"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName CommonFileShare -Type String -Value "\\$SqlServer\$shareName"
-
-        Write-Host "Enable agent logging"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName Enablelogging -Type Dword -Value 1
-
-        Write-Host "Enable agent data upload"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName EnableUpload -Type Dword -Value 1
+    Break
     }
 
     Write-Host 'Link the new GPO titled "Office Telemetry" to the proper OU in your environment.'
-}
+
+
+
