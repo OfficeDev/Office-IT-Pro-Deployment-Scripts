3fbb68b5e487dd5623caf03923339bd35037c05b
Russ Smith <rsmith@valoremconsulting.com>
2015-08-06 23:01:09 -0500

    Merging Configure-GPOOfficeInstallation into master

---
+++ Configure-GPOOfficeInstallation/InstallOffice2013.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+Param
+(
+	[Parameter(Mandatory=$True)]
+	[String]$UncPath,
+	
+	[Parameter(Mandatory=$True)]
+	[String]$ConfigFileName
+)
+
+Set-Location $UncPath
+
+$c2RFileName = "SetupOffice2013.exe"
+$app = ".\$c2RFileName"
+
+$arguments = "/configure", "$ConfigFileName"
+
+& $app @arguments
