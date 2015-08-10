66f57fceb34f3eee0740564b85285ffba6696266
AStigall <astigall@valoremconsulting.com>
2015-08-10 14:14:36 -0500

    Fixed the commonfileshare parameter and suppressed the output after changing a reg key.

--- Create-TelemetryEnvironment/Create-TelemetryGpo.ps1
+++ Create-TelemetryEnvironment/Create-TelemetryGpo.ps1
@@ -4,7 +4,7 @@
     [string]$GpoName,
     
     [Parameter()]
-    [string]$SqlServerName,
+    [string]$CommonFileShare,
 
     [Parameter()]
     [string]$OfficeVersion
@@ -24,27 +24,29 @@ the agent to log and upload.
 .PARAMETER GpoName
 The name of the GPO to be created.
 
-.PARAMETER SqlServerName
-The name of the SQL server hosting the telemetry database.
+.PARAMETER CommonFileShare
+The name of the Shared Drive hosting the telemetry database.
 
 .PARAMETER OfficeVersion
 The version of office used in your environment. If a version
 earlier than 2013 is used do not use this parameter.
 
 .EXAMPLE
-./Create-TelemetryGpo -GpoName "Office Telemetry" -SqlServerName SQLExpress -officeVersion 2013
+./Create-TelemetryGpo -GpoName "Office Telemetry" -CommonFileShare "Server1" -officeVersion 2013
 A GPO named "Office Telemetry" will be created. Registry keys will be
 created to enable telemetry agent logging, uploading, and the commonfileshare 
-path set to \\SQLExpress\TDShared. 
+path set to \\Server1\TDShared. 
 
 .EXAMPLE
-./Create-TelemetryGpo -GpoName "Office Telemetry" -SqlServerName SQLExpress
+./Create-TelemetryGpo -GpoName "Office Telemetry"
 A GPO named "Office Telemetry" will be created.
 
 #>
 
-    Import-Module -Name grouppolicy 
+    Write-Host "Creating a new Group Policy..."
     
+    Import-Module -Name grouppolicy
+
     New-GPO -Name $gpoName
 
     $shareName = "TDShared"
@@ -52,24 +54,23 @@ A GPO named "Office Telemetry" will be created.
     if($OfficeVersion -eq 2013)
     {
         Write-Host "Set the Fileshare name"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName CommonFileShare -Type String -Value "\\$SqlServerName\$shareName"
+        Set-GPRegistryValue -Name $GpoName -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName CommonFileShare -Type String -Value "\\$CommonFileShare\$shareName" | Out-Null
 
         Write-Host "Enable agent logging"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName Enablelogging -Type Dword -Value 1
+        Set-GPRegistryValue -Name $GpoName -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName Enablelogging -Type Dword -Value 1 | Out-Null
 
         Write-Host "Enable agent data upload"
-        Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName EnableUpload -Type Dword -Value 1
-    }
+        Set-GPRegistryValue -Name $GpoName -Key "HKCU\Software\Policies\Microsoft\office\15.0\osm" -ValueName EnableUpload -Type Dword -Value 1 | Out-Null
         elseif($OfficeVersion -eq 2016)
         {
             Write-Host "Set the Fileshare name"
-            Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName CommonFileShare -Type String -Value "\\$SqlServerName\$shareName"
+            Set-GPRegistryValue -Name $GpoName -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName CommonFileShare -Type String -Value "\\$CommonFileShare\$shareName" | Out-Null
 
             Write-Host "Enable agent logging"
-            Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName Enablelogging -Type Dword -Value 1
+            Set-GPRegistryValue -Name $GpoName -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName Enablelogging -Type Dword -Value 1 | Out-Null
 
             Write-Host "Enable agent data upload"
-            Set-GPRegistryValue -Name "Office Telemetry" -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName EnableUpload -Type Dword -Value 1
+            Set-GPRegistryValue -Name $GpoName -Key "HKCU\Software\Policies\Microsoft\office\16.0\osm" -ValueName EnableUpload -Type Dword -Value 1 | Out-Null
         }
     else
     {
