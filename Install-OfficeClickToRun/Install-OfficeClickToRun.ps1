88baf3922e83f2a32c0ddd97f76574b4025667d0
Russ Smith <rsmith@valoremconsulting.com>
2015-08-21 13:37:20 -0500

    Checking in progress

--- Install-OfficeClickToRun/Install-OfficeClickToRun.ps1
+++ Install-OfficeClickToRun/Install-OfficeClickToRun.ps1
@@ -1,5 +1,3 @@
-
-
 Function Get-CurrentOfficeConfiguration {
 <#
 .Synopsis
@@ -51,6 +49,7 @@ param(
 begin {
     $HKLM = [UInt32] "0x80000002"
     $HKCR = [UInt32] "0x80000000"
+    $HKU = [UInt32] "0x80000003"
    
     $installKeys = 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
                    'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
@@ -63,7 +62,6 @@ begin {
     $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]]$defaultDisplaySet)
     $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
 }
-
 
 process {
 
@@ -103,24 +101,37 @@ process {
     [string]$platform = $regProv.GetStringValue($HKLM, $configurationPath, "Platform").sValue
     [string]$clientCulture = $regProv.GetStringValue($HKLM, $configurationPath, "ClientCulture").sValue
     [string]$productIds = $regProv.GetStringValue($HKLM, $configurationPath, "ProductReleaseIds").sValue
-    
-    $productIds = $productIds.Split(',');
+    [string]$versionToReport = $regProv.GetStringValue($HKLM, $configurationPath, "VersionToReport").sValue
+    [string]$updatesEnabled = $regProv.GetStringValue($HKLM, $configurationPath, "UpdatesEnabled").sValue
+    [string]$updateUrl = $regProv.GetStringValue($HKLM, $configurationPath, "UpdateUrl").sValue
+    [string]$updateDeadline = $regProv.GetStringValue($HKLM, $configurationPath, "UpdateDeadline").sValue
 
-    foreach ($productId in $productIds) {       
-       $excludeApps = odtGetExcludedApps -ConfigDoc $ConfigFile -OfficeKeyPath $officeKeyPath -ProductId $productId
-       odtAddProduct -ConfigDoc $ConfigFile -ProductId $productId -ExcludeApps $excludeApps
+    getLanguages -regProv $regProv
+
+    $splitProducts = $productIds.Split(',');
+
+    if ($platform.ToLower() -eq "x86") {
+        $platform = "32"
+    } else {
+        $platform = "64"
+    }
+
+    foreach ($productId in $splitProducts) { 
+       $excludeApps = $NULL
+       if ($productId.ToLower().StartsWith("o365")) {
+           $excludeApps = odtGetExcludedApps -ConfigDoc $ConfigFile -OfficeKeyPath $officeKeyPath -ProductId $productId
+       }
+       odtAddProduct -ConfigDoc $ConfigFile -ProductId $productId -ExcludeApps $excludeApps -Version $versionToReport -Platform $platform
+       odtAddUpdates -ConfigDoc $ConfigFile -Enabled $updatesEnabled -UpdatePath $updateUrl -Deadline $updateDeadline
     }
     
-    
-    
-    $ConfigFile.OuterXml
+    Format-XML ([xml]($ConfigFile)) -indent 4
   }
 
   return $results;
 }
 
 }
-
 
 function odtGetExcludedApps() {
     param(
@@ -155,11 +166,11 @@ function odtGetExcludedApps() {
                if ($installedItem.ToLower().StartsWith($appName.ToLower())) {
                   $appInstalled = $true
                   break;
-              }
+               }
            }
            
            if (!($appInstalled)) {
-              $appsToExclude += $excludeApp
+              $appsToExclude += $appName
            }
         }
         
@@ -169,11 +180,14 @@ function odtGetExcludedApps() {
 
 function odtAddProduct() {
     param(
-       [Parameter(ValueFromPipelineByPropertyName=$true)]
+       [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
        [System.XML.XMLDocument]$ConfigDoc = $NULL,
 
        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
        [string]$ProductId = $NULL,
+
+       [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+       [string]$Platform = $NULL,
 
        [Parameter(ValueFromPipelineByPropertyName=$true)]
        [string]$ClientCulture = "en-us",
@@ -182,7 +196,10 @@ function odtAddProduct() {
        [string[]]$AdditionalLanguages,
 
        [Parameter(ValueFromPipelineByPropertyName=$true)]
-       [string[]] $ExcludeApps
+       [string[]] $ExcludeApps,
+
+       [Parameter(ValueFromPipelineByPropertyName=$true)]
+       [string]$Version = $NULL
     )
 
     [System.XML.XMLElement]$ConfigElement=$NULL
@@ -197,6 +214,14 @@ function odtAddProduct() {
         $ConfigFile.DocumentElement.appendChild($AddElement) | Out-Null
     } else {
         $AddElement = $ConfigFile.Configuration.Add 
+    }
+
+    if ($Version) {
+       $AddElement.SetAttribute("Version", $Version) | Out-Null
+    }
+
+    if ($Platform) {
+       $AddElement.SetAttribute("Edition", $Platform) | Out-Null
     }
 
     [System.XML.XMLElement]$ProductElement = $ConfigFile.Configuration.Add.Product | ?  ID -eq $ProductId
@@ -232,4 +257,113 @@ function odtAddProduct() {
 
 }
 
+function odtAddUpdates{
+
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [System.XML.XMLDocument]$ConfigDoc = $NULL,
+        
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string] $Enabled,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string] $UpdatePath,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string] $TargetVersion,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string] $Deadline
+
+    )
+
+    Process{
+        #Check to make sure the correct root element exists
+        if($ConfigDoc.Configuration -eq $null){
+            throw $NoConfigurationElement
+        }
+
+        #Get the Updates Element if it exists
+        [System.XML.XMLElement]$UpdateElement = $ConfigDoc.Configuration.GetElementsByTagName("Updates").Item(0)
+        if($ConfigDoc.Configuration.Updates -eq $null){
+            [System.XML.XMLElement]$UpdateElement=$ConfigDoc.CreateElement("Updates")
+            $ConfigDoc.Configuration.appendChild($UpdateElement) | Out-Null
+        }
+
+        #Set the desired values
+        if([string]::IsNullOrWhiteSpace($Enabled) -eq $false){
+            $UpdateElement.SetAttribute("Enabled", $Enabled) | Out-Null
+        } else {
+          if ($PSBoundParameters.ContainsKey('Enabled')) {
+              $ConfigDoc.Configuration.Updates.RemoveAttribute("Enabled")
+          }
+        }
+
+        if([string]::IsNullOrWhiteSpace($UpdatePath) -eq $false){
+            $UpdateElement.SetAttribute("UpdatePath", $UpdatePath) | Out-Null
+        } else {
+          if ($PSBoundParameters.ContainsKey('UpdatePath')) {
+              $ConfigDoc.Configuration.Updates.RemoveAttribute("UpdatePath")
+          }
+        }
+
+        if([string]::IsNullOrWhiteSpace($TargetVersion) -eq $false){
+            $UpdateElement.SetAttribute("TargetVersion", $TargetVersion) | Out-Null
+        } else {
+          if ($PSBoundParameters.ContainsKey('TargetVersion')) {
+              $ConfigDoc.Configuration.Updates.RemoveAttribute("TargetVersion")
+          }
+        }
+
+        if([string]::IsNullOrWhiteSpace($Deadline) -eq $false){
+            $UpdateElement.SetAttribute("Deadline", $Deadline) | Out-Null
+        } else {
+          if ($PSBoundParameters.ContainsKey('Deadline')) {
+              $ConfigDoc.Configuration.Updates.RemoveAttribute("Deadline")
+          }
+        }
+
+    }
+}
+
+Function Format-XML ([xml]$xml, $indent=2) { 
+    $StringWriter = New-Object System.IO.StringWriter 
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter 
+    $xmlWriter.Formatting = "indented" 
+    $xmlWriter.Indentation = $Indent 
+    $xml.WriteContentTo($XmlWriter) 
+    $XmlWriter.Flush() 
+    $StringWriter.Flush() 
+    Write-Output $StringWriter.ToString() 
+}
+
+function getLanguages() {
+    param(
+       [Parameter(ValueFromPipelineByPropertyName=$true)]
+       $regProv = $NULL
+    )
+
+  $HKU = [UInt32] "0x80000003"
+
+  #HKEY_USERS\S-1-5-21-3551186017-1269127108-3113414353-1001\Control Panel\Desktop\MuiCached
+
+  $userKeys = $regProv.EnumKey($HKU, "");
+
+  foreach ($userKey in $userKeys.sNames) {
+     if ($userKey.Length -gt 8 -and !($userKey.ToLower().EndsWith("_classes"))) {
+       Write-Host $userKey
+
+       [string]$languagePath = join-path $userKey "Control Panel\Desktop\MuiCached"
+       [string]$userLanguages = $regProv.GetMultiStringValue($HKU, $languagePath, "MachinePreferredUILanguages").sValue
+
+       Write-Host $userLanguages
+     }
+  }
+
+
+}
+
 Get-CurrentOfficeConfiguration
+
