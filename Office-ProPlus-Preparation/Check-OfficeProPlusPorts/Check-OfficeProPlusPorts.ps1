fa34a77fe2b4967129e588136b262438de37e79d
alex182 <alexbaker182@gmail.com>
2015-11-10 11:06:00 -0600

    Added all urls/ports.  Runs Test-NetConnection for each object.  Still need to filter out status of test

--- Office-ProPlus-Preparation/Check-OfficeProPlusPorts/Check-OfficeProPlusPorts.ps1
+++ Office-ProPlus-Preparation/Check-OfficeProPlusPorts/Check-OfficeProPlusPorts.ps1
@@ -42,24 +42,36 @@ process {
 
     $results = new-object PSObject[] 0;
 
-    $results += New-Object PSObject -Property @{Name = "Renew Product Key"; Host = "activation.sls.microsoft.com"; Port = 443; Status = ""; }
-    $results += New-Object PSObject -Property @{Name = "Validate Certificates"; Host = "crl.microsoft.com"; Port = 443; Status = ""; }
-    $results += New-Object PSObject -Property @{Name = "Validate Certificates"; Host = "crl.microsoft.com"; Port = 80; Status = ""; }
+    $results += New-Object PSObject -Property @{Name = "Renew Product Key"; Host = "activation.sls.microsoft.com"; Port = 443; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Validate Certificates"; Host = "crl.microsoft.com"; Port = 443; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Validate Certificates"; Host = "crl.microsoft.com"; Port = 80; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Identity Configuration Services"; Host = "odc.officeapps.live.com"; Port = 443; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Identity Configuration Services"; Host = "clientconfig.microsoftonline-p.net"; Port = 443; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Office Licensing Service"; Host = "ols.officeapps.live.com"; Port = 443; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Redirection Services"; Host = "office15client.microsoft.com"; Port = 443; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Installation/Update Contet"; Host = "officecdn.microsoft.com"; Port = 80; Status = "Fail"; }
+    $results += New-Object PSObject -Property @{Name = "Online Help Services"; Host = "go.microsoft.com"; Port = 80; Status = "Fail"; }
 
-    foreach ($result in $results) {
-
-
-
-
-
-
-    }
 
     foreach ($result in $results) {
       $result | Add-Member MemberSet PSStandardMembers $PSStandardMembers
     }
 
-    return $results;
+    
+    foreach ($result in $results) {
+    
+    $url = $result | select -ExpandProperty Host
+    $port = $result | select -ExpandProperty Port
+
+    $status = Test-NetConnection -ComputerName $url -Port $port
+
+    Write-Output $status
+
+
+    }
+
+
+    #return $results;
 }
 
 }
