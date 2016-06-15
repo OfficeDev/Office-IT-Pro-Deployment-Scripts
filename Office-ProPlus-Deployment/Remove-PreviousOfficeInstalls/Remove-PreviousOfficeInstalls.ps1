9f4220f1de9d0193dba76b88e1f3ae6fd08acab1
Russ Smith <rsmith@valoremconsulting.com>
2016-06-15 13:35:01 -0500

    Changing wscript to cscript

--- Office-ProPlus-Deployment/Remove-PreviousOfficeInstalls/Remove-PreviousOfficeInstalls.ps1
+++ Office-ProPlus-Deployment/Remove-PreviousOfficeInstalls/Remove-PreviousOfficeInstalls.ps1
@@ -352,11 +352,11 @@ Function Remove-PreviousOfficeInstalls{
       Write-Host "Removing Office products..."
 
       if (Test-Path -Path $ActionFile) {
-          wscript $ActionFile
+          cscript $ActionFile
 
           Do{
             Start-Sleep -Seconds 5
-            $cscriptProcess = Get-Process wscript -ErrorAction Ignore
+            $cscriptProcess = Get-Process cscript -ErrorAction Ignore
           }
           Until($cscriptProcess -eq $null)
       } else {
