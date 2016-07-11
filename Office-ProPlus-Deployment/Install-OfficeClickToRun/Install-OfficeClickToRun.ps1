b773f9f1576cf45e9afc1c5f57d55d64530589b6
Russ Smith <rsmith@valoremconsulting.com>
2016-07-11 11:46:48 -0500

    Updating Development

--- Office-ProPlus-Deployment/Install-OfficeClickToRun/Install-OfficeClickToRun.ps1
+++ Office-ProPlus-Deployment/Install-OfficeClickToRun/Install-OfficeClickToRun.ps1
@@ -137,6 +137,8 @@ function Install-OfficeClickToRun {
     StartProcess -execFilePath $cmdLine -execParams $cmdArgs -WaitForExit $false
 
     if ($WaitForInstallToFinish) {
+         Start-Sleep -Seconds 5
+
          Wait-ForOfficeCTRInstall -OfficeVersion $OfficeVersion
     }
 }
@@ -420,7 +422,7 @@ file.
             throw $NoConfigurationElement
         }
         
-        $ConfigFile.Configuration.GetElementsByTagName("Add") | Select OfficeClientEdition, SourcePath, Version
+        $ConfigFile.Configuration.GetElementsByTagName("Add") | Select OfficeClientEdition, SourcePath, Version, Channel, Branch
     }
 
 }
@@ -787,9 +789,9 @@ Function Wait-ForOfficeCTRInstall() {
     }
 
     process {
-       Write-Host "Waiting for Update to Complete..."
+       Write-Host "Waiting for Install to Complete..."
 
-       Start-Sleep -Seconds 20
+       Start-Sleep -Seconds 25
 
        if($OfficeVersion -eq 'Office2016'){
            $mainRegPath = 'SOFTWARE\Microsoft\Office\ClickToRun'
