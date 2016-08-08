75db4c80d591d25f7bd0eb0f38230983c847bea3
Russ Smith <rsmith@valoremconsulting.com>
2016-08-08 14:31:39 -0500

    Removing files from tracking

--- Office-ProPlus-Deployment/Microsoft.ProPlus.InstallGenerator/InstallGenWPF/bin/NET40/Release/Resources/UpdateScriptLaunch.ps1
+++
@@ -1,32 +0,0 @@
-﻿param(
-        [Parameter()]
-        [string] $machineToRun = $Null,
-
-        [Parameter()]
-        [bool] $DisplayLevel = $false,
-
-        [Parameter()]
-        [string] $UpdateToVersion = $NULL,
-
-        [Parameter()]
-        [string] $Channel = $NULL
-        
-    )
-
-    Process{
-    try{
-    if ($PSScriptRoot) {   $scriptPath = $PSScriptRoot } else {   $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition }
-
-
-        $textToWrite = Invoke-Command -ComputerName $machineToRun -FilePath $scriptPath\Update-Office.ps1 -ArgumentList $UpdateToVersion, $Channel, $DisplayLevel
-
-    }
-    catch    {
-    Write-Host $_.Exception.Message
-           throw;
-    }
-    $filename = $machineToRun+"PowershellAttempt.txt"
-    $textToWrite | Out-File $env:temp\$filename
-
-    #Stop-Process -Id $PID
-    }
