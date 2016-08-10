4a3c9b77ff8c538f3d08a39a6598c5e7f3e789d0
Russ Smith <rsmith@valoremconsulting.com>
2016-08-10 09:57:46 -0500

    Pushing changes for the Deployment folder to master for the Deploy-OfficeClickToRun folder

--- Office-ProPlus-Deployment/Deploy-OfficeClickToRun/6-ExampleDeployWithDynamicSourcePath.ps1
+++ Office-ProPlus-Deployment/Deploy-OfficeClickToRun/6-ExampleDeployWithDynamicSourcePath.ps1
@@ -9,7 +9,6 @@ Process {
  if ($PSScriptRoot) {
    $scriptPath = $PSScriptRoot
  } else {
-   $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
    $scriptPath = (Get-Item -Path ".\").FullName
  }
 
@@ -26,7 +25,7 @@ $targetFilePath = "$env:temp\configuration.xml"
 #from which the script is run.  It will then remove the Version attribute from the XML to ensure the installation gets the latest version
 #when updating an existing install and then it will initiate a install
 
-Generate-ODTConfigurationXml -Languages AllInUseLanguages -TargetFilePath $targetFilePath | Set-ODTAdd -Version $NULL | Dynamic-UpdateSource | Install-OfficeClickToRun
+Generate-ODTConfigurationXml -Languages AllInUseLanguages -TargetFilePath $targetFilePath | Set-ODTAdd -Version $NULL -Channel Deferred | Dynamic-UpdateSource | Install-OfficeClickToRun
 
 # Configuration.xml file for Click-to-Run for Office 365 products reference. https://technet.microsoft.com/en-us/library/JJ219426.aspx
 }
--- Office-ProPlus-Deployment/Deploy-OfficeClickToRun/Install-OfficeClickToRun.ps1
+++ Office-ProPlus-Deployment/Deploy-OfficeClickToRun/Install-OfficeClickToRun.ps1
@@ -134,7 +134,7 @@ function Install-OfficeClickToRun {
 
     Write-Host "Installing Office Click-To-Run..."
 
-    StartProcess -execFilePath $cmdLine -execParams $cmdArgs -WaitForExit $true
+    StartProcess -execFilePath $cmdLine -execParams $cmdArgs -WaitForExit $false
 
     if ($WaitForInstallToFinish) {
          Start-Sleep -Seconds 5
@@ -327,7 +327,7 @@ Language and Exclude values
 
         if($PSCmdlet.ParameterSetName -eq "All"){
             foreach($ProductElement in $ConfigFile.Configuration.Add.Product){
-                $Result = New-Object ﾂ傍ypeName PSObject 
+                $Result = New-Object 傍ypeName PSObject 
 
                 Add-Member -InputObject $Result -MemberType NoteProperty -Name "ProductId" -Value ($ProductElement.GetAttribute("ID"))
 
@@ -353,7 +353,7 @@ Language and Exclude values
                 $tempId = $ProductElement.GetAttribute("ID")
                 
                 
-                $Result = New-Object ﾂ傍ypeName PSObject 
+                $Result = New-Object 傍ypeName PSObject 
                 Add-Member -InputObject $Result -MemberType NoteProperty -Name "ProductId" -Value $tempId 
                 if($ProductElement.Language -ne $null){
                     $ProductLangs = $configfile.Configuration.Add.Product.Language | % {$_.ID}
@@ -544,7 +544,7 @@ Here is what the portion of configuration file looks like when modified by this 
             Write-Host "The Office XML Configuration file has been saved to: $TargetFilePath"
         } else {
             $results = new-object PSObject[] 0;
-            $Result = New-Object ﾂ傍ypeName PSObject 
+            $Result = New-Object 傍ypeName PSObject 
             Add-Member -InputObject $Result -MemberType NoteProperty -Name "TargetFilePath" -Value $TargetFilePath
             Add-Member -InputObject $Result -MemberType NoteProperty -Name "Level" -Value $Level
             Add-Member -InputObject $Result -MemberType NoteProperty -Name "AcceptEULA" -Value $AcceptEULA
@@ -761,7 +761,7 @@ Here is what the portion of configuration file looks like when modified by this 
             Write-Host "The Office XML Configuration file has been saved to: $TargetFilePath"
         } else {
             $results = new-object PSObject[] 0;
-            $Result = New-Object ﾂ傍ypeName PSObject 
+            $Result = New-Object 傍ypeName PSObject 
             Add-Member -InputObject $Result -MemberType NoteProperty -Name "TargetFilePath" -Value $TargetFilePath
             Add-Member -InputObject $Result -MemberType NoteProperty -Name "ProductId" -Value $ProductId
             Add-Member -InputObject $Result -MemberType NoteProperty -Name "LanguageIds" -Value $LanguageIds
@@ -858,11 +858,6 @@ Function Wait-ForOfficeCTRInstall() {
               }
            }
 
-
-           if ($allComplete) {
-              break;
-           }
-
            if ($startTime -lt (Get-Date).AddHours(-$TimeOutInMinutes)) {
               throw "Waiting for Update Timed-Out"
               break;
@@ -871,18 +866,19 @@ Function Wait-ForOfficeCTRInstall() {
            Start-Sleep -Seconds 5
        } while($updateRunning -eq $true) 
 
-       if ($updateRunning) {
+       if((!$updateRunning) -and $trackProgress.Count -gt '0') {
           if ($failure) {
-            Write-host ""
-            Write-Host "Update Failed"
-          } else {
-            Write-host ""
-            Write-Host "Update Complete"
+              Write-host ""
+              Write-Host "Update Failed"
+              break;
+          }else {
+              Write-host ""
+              Write-Host "Update Complete"
           }
-       } else {
-          Write-host ""
-          Write-Host "Update Not Running"
-       } 
+       }else {
+           Write-host ""
+           Write-Host "Update Not Running"
+       }
     }
 }
 
@@ -900,7 +896,7 @@ function showTaskStatus() {
     )
 
     $results = new-object PSObject[] 0;
-    $Result = New-Object ﾂ傍ypeName PSObject 
+    $Result = New-Object 傍ypeName PSObject 
     Add-Member -InputObject $Result -MemberType NoteProperty -Name "Operation" -Value $Operation
     Add-Member -InputObject $Result -MemberType NoteProperty -Name "Status" -Value $Status
     Add-Member -InputObject $Result -MemberType NoteProperty -Name "DateTime" -Value $DateTime
