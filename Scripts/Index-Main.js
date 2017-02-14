970c4bf542b249e3622322f419f566271da1cb94
alex182 <alexbaker182@gmail.com>
2017-02-14 12:38:47 -0600

    added first readme to faq

--- Scripts/Index-Main.js
+++ Scripts/Index-Main.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function() {
 
+    checkAddress();
+   sideBarHeight();
 });
 
 function openInNewTab(url) {
@@ -15,8 +17,70 @@ function downloadZip(){
 function toggleSection(item){
 
     var section_body = $(item).parent().children('.Section-Body') 
+    var chevron = $(item).parent().children('.Section-Header').children('.section-Header-Chevron'); 
 
-    section_body.toggleClass('ms-u-slideUpIn10 ms-u-slideUpOut10')
+    if(section_body.hasClass('ms-u-slideDownIn10')){
+        section_body.removeClass('ms-u-slideDownIn10');
+        section_body.addClass('ms-u-slideUpOut10');
+        section_body.addClass('hidden');
+
+        chevron.removeClass('open-Chevron');
+        chevron.addClass('close-Chevron');
+
+
+    }else if(section_body.hasClass('hidden') || section_body.hasClass('ms-u-slideUpOut10')){
+        var parentId =  $(item).parent().children('.Section-Header').attr('id') 
+        section_body.removeClass('hidden');
+        section_body.addClass('ms-u-slideDownIn10');
+        section_body.removeClass('ms-u-slideUpOut10');
+
+        chevron.removeClass('close-Chevron');
+        chevron.addClass('open-Chevron');
+    }
+    sideBarHeight();
+}
+
+function focusSection(sectionId,item){
+
+    location.hash = '';
+    location.hash = '#'+sectionId;   
+
+     $('html, body').animate({
+        scrollTop: $('#'+sectionId).offset().top
+    }, 500);
+    
+
+    $('#'+sectionId).click();
+
+    $('.Nav-Option').each(function(i,obj){
+        $(obj).removeClass('selected');
+    });
+
+    $(item).addClass('selected');
+    sideBarHeight();
+}
+
+function sideBarHeight(){
+     $('#siteNav').parents().each(function(i,obj){ 
+        $(obj).height('100%');
+    });
+
+    $('#siteNav').height($('body').height());
+    $('#siteNav').height($(document).height());
+}
+
+function checkAddress(){
+    var sectionId = location.hash.replace('#','');
+
+    if(sectionId){
+         $('.Nav-Option').each(function(i,obj){
+        var onclickvalue = ($(obj).attr('onClick'))
+        
+        if(onclickvalue.indexOf(sectionId) > -1){
+            $(obj).click();
+        }
+    });
+    }  
 }
 
 
