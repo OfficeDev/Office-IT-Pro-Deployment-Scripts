b3f47ea5967202451fc61ee38aaa57590efbdf50
Adam Stigall <astigall@valoremconsulting.com>
2017-03-02 20:01:01 -0600

    Adding new function to generate LanguagePack deployment files

---
+++ Office-ProPlus-Deployment/Generate-ODTLanguagePackXML/README.md
@@ -0,0 +1,43 @@
+﻿#**Generate Office Deployment Tool (ODT) LanguagePack XML**
+
+This PowerShell function will create a new XML file that can be used to deploy additional languages packs to a computer with Office 365 ProPlus already installed. This XML is used with the [Office Deployment Tool (ODT)](http://www.microsoft.com/en-us/download/details.aspx?id=36778) to deploy Office Click-To-Run products.    
+
+Deploying Office language packs can be challenging in organizations that have to support many different languages.  This script provides a way to automate the deployment of Language Packs.  
+
+You can control the bit and language IDs that are needed for the deployment.
+
+For more information on the specifics of deploying Office 365 ProPlus language packs go to the following link.
+[Add languages to existing installations of Office 365 ProPlus](https://technet.microsoft.com/en-us/library/jj219422.aspx#Anchor_7)
+
+###**Examples**
+
+1. Open a PowerShell console.
+
+		From the Run dialog type PowerShell 
+
+2. Change directory to the location where the PowerShell Script is saved.
+
+		Example: cd C:\PowerShellScripts
+
+3. Dot-Source the Generate-ODTLanguagePackXML function into your current session.
+
+		Type . .\Generate-ODTLanguagePackXML.ps1
+		By including the additional period before the relative script path you are 'Dot-Sourcing' 
+		the PowerShell function in the script into your PowerShell session which will allow you to 
+		run the function from the console.
+
+4. Run the function
+
+		Generate-ODTLanguagePackXml -Languages es-es,de-de,fr-fr -TargetFilePath LanguagePacks.xml 
+
+5. An example output is below.  
+
+          <Configuration>
+             <Add OfficeClientEdition="32">
+                 <Product ID="LanguagePack">
+                   <Language ID="es-es" />
+                   <Language ID="de-de" />
+                   <Language ID="fr-fr" />
+                 </Product>
+             </Add>
+          </Configuration>
---
+++ Office-ProPlus-Deployment/Setup-CMOfficeDeployment/DeploymentFiles/Generate-ODTLanguagePackXML.ps1
@@ -0,0 +1,269 @@
+﻿function Generate-ODTLanguagePackXML {
+<#
+.SYNOPSIS
+Create an ODT configuration file to deploy additional language packs
+
+.DESCRIPTION
+This script will create a new xml file that should be used to deploy additional 
+language packs to computers with Office 365 ProPlus already installed.
+
+.PARAMETER TargetFilePath
+The full path where to save the file.
+
+.PARAMETER OfficeClientEdition
+The bit of Office. Choose between 32 and 64.
+
+.PARAMETER Languages
+The list of available languages.
+
+.EXAMPLE
+Generate-ODTLanguagePackXML -TargetFilePath $env:temp\LanguagePacks.xml -Languages de-de,es-es,fr-fr -OfficeClientEdition 64
+A new xml file will be created in the temp directory called LanguagePacks.xml which will be used to install the 64-bit
+editions of German, Spanish, and French language packs.
+
+.EXAMPLE
+Generate-ODTLanguagePackXML -TargetFilePath $env:temp\LanguagePacks.xml -Languages de-de,es-es,fr-fr | fl
+A new xml file will be created in the temp directory called LanguagePacks.xml which will be used to install the 32-bit
+editions of German, Spanish, and French language packs. The output of the xml file will be displayed on the PowerShell console.
+
+.NOTES
+Date created: 03-02-2017
+Date modified: 03-02-2017
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+param(
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [String]$TargetFilePath = $NULL,
+
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [ValidateSet("32","64")]
+    [string]$OfficeClientEdition = '32',
+
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [ValidateSet("en-us","ar-sa","bg-bg","zh-cn","zh-tw","hr-hr","cs-cz","da-dk","nl-nl","et-ee","fi-fi","fr-fr","de-de","el-gr","he-il","hi-in","hu-hu","id-id","it-it",
+                "ja-jp","kk-kz","ko-kr","lv-lv","lt-lt","ms-my","nb-no","pl-pl","pt-br","pt-pt","ro-ro","ru-ru","sr-latn-rs","sk-sk","sl-si","es-es","sv-se","th-th",
+                "tr-tr","uk-ua","vi-vn")] 
+    [string[]]$Languages
+)
+
+begin {   
+    [string]$tempStr = $MyInvocation.MyCommand.Path
+    $scriptPath = GetScriptPath     
+}
+
+process{
+    if ($TargetFilePath) {
+        $folderPath = Split-Path -Path $TargetFilePath -Parent
+        $fileName = Split-Path -Path $TargetFilePath -Leaf
+        if ($folderPath) {
+            [system.io.directory]::CreateDirectory($folderPath) | Out-Null
+        }
+    }
+
+    [System.XML.XMLDocument]$ConfigFile = New-Object System.XML.XMLDocument
+   
+    #Generate the language pack xml file
+    odtAddLanguagePackProduct -ConfigDoc $ConfigFile -Platform $OfficeClientEdition -LanguageIds $Languages
+
+    $formattedXml = Format-XML ([xml]($ConfigFile)) -indent 4
+
+    if ($TargetFilePath) {
+       $formattedXml | Out-File -FilePath $TargetFilePath
+    }
+
+    #Display the results to the console
+    $Result = New-Object -TypeName PSObject 
+    Add-Member -InputObject $Result -MemberType NoteProperty -Name "TargetFilePath" -Value $TargetFilePath
+    Add-Member -InputObject $Result -MemberType NoteProperty -Name "LanguageIds" -Value $Languages
+    Add-Member -InputObject $Result -MemberType NoteProperty -Name "ConfigurationXML" -Value $formattedXml
+    $Result
+}
+
+}
+
+function odtAddLanguagePackProduct() {
+    param(
+       [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+       [System.XML.XMLDocument]$ConfigDoc = $NULL,
+
+       [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+       [string]$ProductId = "LanguagePack",
+
+       [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+       [string]$Platform = $NULL,
+
+       [Parameter(ValueFromPipelineByPropertyName=$true)]
+       [string[]]$LanguageIds = @()
+    )
+
+    [System.XML.XMLElement]$ConfigElement=$NULL
+    if($ConfigDoc.Configuration -eq $null){
+        $ConfigElement=$ConfigDoc.CreateElement("Configuration")
+        $ConfigDoc.appendChild($ConfigElement) | Out-Null
+    }
+
+    [System.XML.XMLElement]$AddElement=$NULL
+    if($ConfigFile.Configuration.Add -eq $null){
+        $AddElement=$ConfigDoc.CreateElement("Add")
+        $ConfigDoc.DocumentElement.appendChild($AddElement) | Out-Null
+    } else {
+        $AddElement = $ConfigDoc.Configuration.Add 
+    }
+
+    if ($Platform) {
+       $AddElement.SetAttribute("OfficeClientEdition", $Platform) | Out-Null
+    }
+
+    [System.XML.XMLElement]$ProductElement = $ConfigDoc.Configuration.Add.Product | where { $_.ID -eq $ProductId }
+    if($ProductId){
+        if($ProductElement -eq $null){
+            [System.XML.XMLElement]$ProductElement=$ConfigDoc.CreateElement("Product")
+            $AddElement.appendChild($ProductElement) | Out-Null
+            $ProductElement.SetAttribute("ID", $ProductId) | Out-Null
+        }
+    }
+
+    foreach($LanguageId in $LanguageIds){    
+       if ($LanguageId) {
+          if ($LanguageId.Length -gt 0) {
+            [System.XML.XMLElement]$LanguageElement = $ProductElement.Language | where { $_.ID -eq $LanguageId }
+            if($LanguageElement -eq $null){
+                [System.XML.XMLElement]$LanguageElement=$ConfigFile.CreateElement("Language")
+                $ProductElement.appendChild($LanguageElement) | Out-Null
+                $LanguageElement.SetAttribute("ID", $LanguageId.ToString().ToLower()) | Out-Null
+            }
+          }
+       }
+    }
+}
+
+Function Remove-ODTProductToAdd{
+<#
+.SYNOPSIS
+Removes an existing product to add from the configuration file
+
+.PARAMETER ProductId
+Required. ID must be set to a valid ProductRelease ID.
+See https://support.microsoft.com/en-us/kb/2842297 for valid ids.
+
+.PARAMETER TargetFilePath
+Full file path for the file to be modified and be output to.
+
+.Example
+Remove-ODTProductToAdd -ProductId "O365ProPlusRetail" -TargetFilePath "$env:Public/Documents/config.xml"
+Removes the ProductToAdd with the ProductId 'O365ProPlusRetail' from the XML Configuration file
+
+</Configuration>
+
+#>
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true, Position=0)]
+        [string] $ConfigurationXML = $NULL,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string[]] $ProductId = "Unknown",
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string] $TargetFilePath,
+
+        [Parameter(ParameterSetName="All", ValueFromPipelineByPropertyName=$true)]
+        [switch] $All
+    )
+
+    Process{
+        $TargetFilePath = GetFilePath -TargetFilePath $TargetFilePath
+
+        foreach($Product in $ProductId){
+            if ($Product -eq "Unknown") {
+                $Product = SelectProductId
+            }
+
+            $Product = IsValidProductId -ProductId $Product
+
+            #Load the file
+            [System.XML.XMLDocument]$ConfigFile = New-Object System.XML.XMLDocument
+
+            if ($TargetFilePath) {
+               $ConfigFile.Load($TargetFilePath) | Out-Null
+            } else {
+                if ($ConfigurationXml) 
+                {
+                  $ConfigFile.LoadXml($ConfigurationXml) | Out-Null
+                  $global:saveLastConfigFile = $NULL
+                  $global:saveLastFilePath = $NULL
+                }
+            }
+
+            $global:saveLastConfigFile = $ConfigFile.OuterXml
+
+            #Check that the file is properly formatted
+            if($ConfigFile.Configuration -eq $null){
+                throw $NoConfigurationElement
+            }
+
+            if($ConfigFile.Configuration.Add -eq $null){
+                throw $NoAddElement
+            }
+
+            if (!($All)) {
+                #Set the desired values
+                [System.XML.XMLElement]$ProductElement = $ConfigFile.Configuration.Add.Product | Where { $_.ID -eq $Product }
+                if($ProductElement -ne $null){
+                    $ConfigFile.Configuration.Add.removeChild($ProductElement) | Out-Null
+                }
+
+                if ($ConfigFile.Configuration.Add.Product.Count -eq 0) {
+                    [System.XML.XMLElement]$AddNode = $ConfigFile.SelectSingleNode("/Configuration/Add")
+                    if ($AddNode) {
+                        $ConfigFile.Configuration.removeChild($AddNode) | Out-Null
+                    }
+                }
+            } else {
+               $ConfigFile.Configuration.Add.RemoveAll() | Out-Null
+           
+            }
+        
+            $ConfigFile.Save($TargetFilePath) | Out-Null
+            $global:saveLastFilePath = $TargetFilePath
+
+            if (($PSCmdlet.MyInvocation.PipelineLength -eq 1) -or `
+                ($PSCmdlet.MyInvocation.PipelineLength -eq $PSCmdlet.MyInvocation.PipelinePosition)) {
+                Write-Host
+
+                Format-XML ([xml](cat $TargetFilePath)) -indent 4
+
+                Write-Host
+                Write-Host "The Office XML Configuration file has been saved to: $TargetFilePath"
+            } else {
+                $results = new-object PSObject[] 0;
+                $Result = New-Object –TypeName PSObject 
+                Add-Member -InputObject $Result -MemberType NoteProperty -Name "TargetFilePath" -Value $TargetFilePath
+                $Result
+            }
+        }
+    }
+}
+
+Function GetScriptPath() {
+    [string]$scriptPath = "."
+    
+    if ($PSScriptRoot) {
+      $scriptPath = $PSScriptRoot
+    } else {
+      $scriptPath = (Get-Item -Path ".\").FullName
+    }
+    
+    return $scriptPath
+}
+
+function Format-XML ([xml]$xml, $indent=2) { 
+    $StringWriter = New-Object System.IO.StringWriter 
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter 
+    $xmlWriter.Formatting = "indented" 
+    $xmlWriter.Indentation = $Indent 
+    $xml.WriteContentTo($XmlWriter) 
+    $XmlWriter.Flush() 
+    $StringWriter.Flush() 
+    Write-Output $StringWriter.ToString() 
+}
