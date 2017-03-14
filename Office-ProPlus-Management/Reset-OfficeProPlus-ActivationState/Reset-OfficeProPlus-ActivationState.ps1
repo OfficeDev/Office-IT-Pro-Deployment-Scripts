fa221f520dd98e02f10cb7ce7eea6d142d3f3d29
Srinivasan Rajagopalan <Srinivasan.Rajagopal@identitymine.com>
2017-03-13 23:42:54 -0700

    Updates to base version. Feature complete.

--- Office-ProPlus-Management/Reset-OfficeProPlus-ActivationState/Reset-OfficeProPlus-ActivationState.ps1
+++ Office-ProPlus-Management/Reset-OfficeProPlus-ActivationState/Reset-OfficeProPlus-ActivationState.ps1
@@ -26,7 +26,7 @@ https://github.com/OfficeDev/Office-IT-Pro-Deployment-Scripts
 Name: Reset-OfficeProPlus-ActivationState
 Version: 1.0.0
 DateCreated: 2017-03-06
-DateUpdated: 2017-03-08
+DateUpdated: 2017-03-13
 
 
 
@@ -40,16 +40,37 @@ param(
 begin {
 
 	 #ToDo: Replace with a windows temp path later. 
-	 $logPath = "D:\Powershell\log.txt"
+	 $logPath = -join($env:WINDIR,'\Temp\OSPPCleanupLog.txt')
 
 	 $HKCU = 'HKEY_CURRENT_USER'
 	 $HKU = 'HKEY_USERS'
+
 	 
 	 $identitiesFolder = 'SOFTWARE\Microsoft\Office\15.0\Common\Identity\Identities',
                          'SOFTWARE\Microsoft\Office\16.0\Common\Identity\Identities'
 						 
-	 $legacyOSPPvbsFilePath = "'C:\Program Files\Microsoft Office\Office15\ospp.vbs'"
-	 
+	 $checkOSPPvbsFilePathOffice15 = -join($env:ProgramW6432,'\Microsoft Office\Office15\ospp.vbs')
+     $checkOSPPvbsFilePathx86Office15 = -join(${env:ProgramFiles(x86)},'\Microsoft Office\Office15\ospp.vbs')
+     $checkOSPPvbsFilePathOffice16 = -join($env:ProgramW6432,'\Microsoft Office\Office16\ospp.vbs')
+     $checkOSPPvbsFilePathx86Office16 = -join(${env:ProgramFiles(x86)},'\Microsoft Office\Office16\ospp.vbs')
+
+     if(Test-Path $checkOSPPvbsFilePathOffice15)
+     {
+        $legacyOSPPvbsFilePath = $checkOSPPvbsFilePathOffice15
+     }
+     elseif(Test-Path $checkOSPPvbsFilePathx86Office15)
+     {
+        $legacyOSPPvbsFilePath = $checkOSPPvbsFilePathx86Office15
+     }
+     elseif(Test-Path $checkOSPPvbsFilePathOffice16)
+     {
+        $legacyOSPPvbsFilePath = $checkOSPPvbsFilePathOffice16
+     }
+     elseif(Test-Path $checkOSPPvbsFilePathx86Office16)
+     {
+        $legacyOSPPvbsFilePath = $checkOSPPvbsFilePathx86Office16
+     }
+ 
 	 $SID = (New-Object System.Security.Principal.NTAccount($UserName)).Translate([System.Security.Principal.SecurityIdentifier]).value
 	 
 	 $locationstoClear = -join($env:APPDATA,'\Microsoft\Credentials'),
@@ -67,34 +88,50 @@ begin {
 
 
 process {	
+
+    #ToDo: 1. Path validation, 2. Change file path to C:\windows\temp, 3. Parse the output file 
+     
+
 	#Part 1: Remove Office 365 license for Subscription based installs 
-	[string]$Action = "`"cmd /C %systemroot%\system32\cscript.exe $legacyOSPPvbsFilePath /dstatus`""
-	
-	Invoke-Expression "$Action" -OutVariable out | Tee-Object -FilePath $logPath
-	
+
+    #Write results of ospp.vbs to a temp file
+    cmd /C cscript $legacyOSPPvbsFilePath /dstatus > $logPath
+
+    $reader = [System.IO.File]::OpenText($logPath)
+    try
+    {
+        while($null -ne ($line = $reader.ReadLine())) {
+            if ($line.Contains('Last 5 characters of installed product key: ')) 
+            { 
+                $target = $line.Replace('Last 5 characters of installed product key: ','');      
+            
+                #Command to uninstall product key
+                cmd /C cscript $legacyOSPPvbsFilePath /unpkey:$target  
+            }
+        }
+    }
+    finally
+    {
+        $reader.Close();
+    }
+
 
     #Part 2: Remove cached identities from HKCU registry:
     foreach ($identity in $identitiesFolder) {
 	
 		#2a. Remove from HKCU
 		$fullPath = join-path $HKCU $identity
-        
-		#Not deleting at the moment to not screw up the local system.
-		#To Do: Enable below line and remove the following line
-		#Get-ChildItem -Path Registry::$fullPath -Recurse | remove-item -Confirm:$false
-        Get-ChildItem -Path Registry::$fullPath
-		
+       
+		Get-ChildItem -Path Registry::$fullPath -EA SilentlyContinue | remove-item -Confirm:$false -Force -Recurse		
 		Write-Host "Removed all identities from " $fullPath
+        
 		
 		#2b. Remove from HKU for Shared Users
 		$fullPath = [io.path]::combine($HKU, $SID, $identity)
 		
-		#Not deleting at the moment to not screw up the local system.
-		#To Do: Enable below line and remove the following line
-		#Get-ChildItem -Path Registry::$fullPath -Recurse | remove-item -Confirm:$false
-        Get-ChildItem -Path Registry::$fullPath
-		
+		Get-ChildItem -Path Registry::$fullPath -EA SilentlyContinue | remove-item -Confirm:$false -Force -Recurse
 		Write-Host "Removed all identities from " $fullPath
+        
 		
     }
 	
@@ -111,11 +148,8 @@ process {
 					if ($Line.Contains('MicrosoftOffice15') -or $Line.Contains('MicrosoftOffice16')) 
 					{
 						$Target = $Line.Replace('Target: ',''); 
+						cmdkey /delete: $Target 
 						
-						#Not removing at the moment to not screw up the local system.
-						#To Do: Enable below line and remove the following line
-						#cmdkey /delete: $Target 
-						cmdkey /list: $Target
 					}
 				}
 			}
@@ -125,23 +159,20 @@ process {
 	#4a. Registry
 	foreach ($folder in $registrytoClear) 
 	{
-		#Not deleting at the moment to not screw up the local system.
-		#To Do: Enable below line and remove the following line
-		#Get-ChildItem -Path Registry::$folder -Recurse | remove-item -Confirm:$false
-        Get-ChildItem -Path Registry::$folder
-		
+        
+		Remove-ItemProperty -Path Registry::$folder -Name * -EA SilentlyContinue
 		Write-Host "Removed all entries from " $folder
+        
 	}
 	
 	#4b. Folders
 	foreach ($folder in $locationstoClear) 
 	{
-		#Not deleting at the moment to not screw up the local system.
-		#To Do: Enable below line and remove the following line
-		#Get-ChildItem -Path $folder -Recurse | remove-item -Confirm:$false
-        Get-ChildItem -Path $folder
-		
-		Write-Host "Removed all entries from " $folder
+        if(Test-Path $folder)
+        {
+		    Get-ChildItem -Path $folder -EA SilentlyContinue | remove-item -Confirm:$false -Force -Recurse
+		    Write-Host "Removed all entries from " $folder
+        }
 	}
 }
 
